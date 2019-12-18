483ceade36613a3c35b638aa34fc8e3dbcffe56f
deep <hereisdanbrown@gmail.com>
2019-12-17 20:12:55 -0700

    I added my setTime and visualizeEl functions. The timer counts down from 60. The visualizeEl determines what appears on the screen for different events. The start button works. The answers each console.log their values.

--- myjava.js
+++ myjava.js
@@ -1,6 +1,9 @@
 
 var qHDisp = $(".questionHDisplay");
 var qDisp = $(".questionDisplay");
+var tDisp = $(".timer");
+var score = 0;
+var secondsLeft = 60;
 
 // Make this appear along with the questions
 qHDisp.attr("style", "display: none");
@@ -8,21 +11,60 @@ qDisp.attr("style", "display: none");
 
 
 // Buttons
-$(".btn-secondary").on("click", function(event){
-    event.preventDefault();
+var startBtn = $(".btn-primary");
+var ansBtn = $(".btn-outline-secondary");
+var highBtn = $(".btn-secondary");
 
-    console.log("I'm a secondary");
+startBtn.on("click", function(event){
+    event.preventDefault();
+    setTime();
+    console.log("I'm a start");
 });
 
-$(".btn-primary").on("click", function(event){
+ansBtn.on("click", function(event){
     event.preventDefault();
-
-    console.log("I'm a primary");
+    console.log("I'm " + $(this).attr("value"));
 });
 
-// View High Scores
-$(".navbar-brand").on("click", function(event){
+highBtn.on("click", function(event){
     event.preventDefault();
-
-    console.log("I'm the navbar")
+    console.log("I'm a high score button");
 });
+
+// Timer
+function setTime() {
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      visualizeEl("start");
+      // Need this to end if we run out of questions
+      if(secondsLeft === 0) {
+        clearInterval(timerInterval);
+        visualizeEl("score");
+      }
+  
+    }, 1000);
+};
+
+function visualizeEl(event) {
+    switch(event) {
+        case "start":
+            secondsLeft = 60;
+            tDisp.text("Timer: " + secondsLeft + "s");
+            startBtn.attr("style", "display: none");
+            qDisp.attr("style", "display: block");
+            qHDisp.attr("style", "display: block");
+            break;
+        case "score":
+            tDisp.text("Timer");
+            startBtn.attr("style", "display: none");
+            qDisp.attr("style", "display: none");
+            // qHDisp.attr("style", "display: block");
+            qHDisp.text("Your Score:");  
+            // logScore(); 
+            break;         
+
+        
+        // startBtn.setAttribute("style", "display: block");
+        // quizQuestions.setAttribute("style", "display: none");
+    }
+}
